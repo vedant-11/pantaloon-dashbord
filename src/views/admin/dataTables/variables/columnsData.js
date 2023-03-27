d20687a3be3182ccbf81433d3aa75a65ad62760e
vedant-11 <v.vedantsingh.11@gmail.com>
2023-03-27 09:58:13 +0530

    added the code

---
+++ src/views/admin/dataTables/variables/columnsData.js
@@ -0,0 +1,75 @@
+export const columnsDataDevelopment = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "TECH",
+    accessor: "tech",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+  {
+    Header: "MRP",
+    accessor: "MRP",
+  },
+];
+
+export const columnsDataCheck = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "MRP",
+    accessor: "MRP",
+  },
+  {
+    Header: "QUANTITY",
+    accessor: "quantity",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+];
+
+export const columnsDataColumns = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "MRP",
+    accessor: "MRP",
+  },
+  {
+    Header: "QUANTITY",
+    accessor: "quantity",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+];
+
+export const columnsDataComplex = [
+  {
+    Header: "NAME",
+    accessor: "name",
+  },
+  {
+    Header: "STATUS",
+    accessor: "status",
+  },
+  {
+    Header: "DATE",
+    accessor: "date",
+  },
+  {
+    Header: "MRP",
+    accessor: "MRP",
+  },
+];
